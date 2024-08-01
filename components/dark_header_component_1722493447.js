68b8999c2e7b139424ca9da610cd31c30db11809
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:24:19 +0000

    Merge code from frontend

--- components/dark_header_component_1722493447.js
+++ components/dark_header_component_1722493447.js
@@ -4,7 +4,7 @@
 
 Vue.component("dark_header_component_1722493447", {
     template: `
-    <header id="header-component" class="z-30 bg-white">
+    <header id="header-component" style="min-height: 70px" class="z-30 bg-white">
         <div class="max-w-xl lg:max-w-[calc(50%+theme(maxWidth.xl))] mx-auto px-4 sm:px-6">
             <div class="flex items-center justify-between h-16 md:h-20">
                 <!-- Site branding -->
